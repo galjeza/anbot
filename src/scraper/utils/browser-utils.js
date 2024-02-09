a0db1057aa15191d5ac6d9de9a76be49176a0471
galjeza <gal.jeza@protonmail.com>
2024-02-09 18:02:14 +0100

    broker id in post

--- src/scraper/utils/browser-utils.js
+++ src/scraper/utils/browser-utils.js
@@ -1,14 +1,16 @@
-import puppeteerVanilla from 'puppeteer';
-import { addExtra } from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import UserAgentPlugin from 'puppeteer-extra-plugin-anonymize-ua';
-
 import puppeteer from 'puppeteer-extra';
 puppeteer.use(UserAgentPlugin());
 puppeteer.use(StealthPlugin());
 
+function getChromiumExecPath() {
+  return puppeteer.executablePath().replace('app.asar', 'app.asar.unpacked');
+}
+
 export async function setupBrowser() {
   const browser = await puppeteer.launch({
+    executablePath: getChromiumExecPath(),
     headless: false,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
