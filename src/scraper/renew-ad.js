e95463ba3b3c6f4b5431e0911539d0ea3cfb9932
galjeza <gal.jeza@protonmail.com>
2024-02-10 17:52:08 +0100

    Fix deleteOldAd function to get the page before navigating

--- src/scraper/renew-ad.js
+++ src/scraper/renew-ad.js
@@ -250,6 +250,8 @@ const uploadImages = async (browser, carData) => {
 };
 
 const deleteOldAd = async (browser, adId) => {
+  const [page] = await browser.pages();
+
   await page.goto(
     `https://www.avto.net/_2016mojavtonet/ad_delete.asp?id=${adId}`,
   );
