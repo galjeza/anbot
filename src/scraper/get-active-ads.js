a31aee281fd04e9be85127f7fc9ddfd5e03884b9
Gal Jeza <gal.jeza55@gmail.com>
2025-10-01 16:44:34 +0200

    remove top level try catch in fetchactiveads

--- src/scraper/get-active-ads.js
+++ src/scraper/get-active-ads.js
@@ -1,84 +1,73 @@
 import { setupBrowser } from './utils/browser-utils.js';
 import { AVTONET_URLS } from './utils/constants.js';
-import { saveList } from './utils/utils.js';
 
 export async function fetchActiveAds(brokerId, adType) {
   const url = AVTONET_URLS[adType] + brokerId;
   const browser = await setupBrowser();
   const [page] = await browser.pages();
+  await page.goto(url);
 
-  try {
-    await page.goto(url);
+  await page.waitForSelector('.GO-Results-Row');
+  let adElements = await page.$$('.GO-Results-Row');
+  const adData = [];
 
-    await page.waitForSelector('.GO-Results-Row');
-    let adElements = await page.$$('.GO-Results-Row');
-    const adData = [];
+  let scrapePage = true;
 
-    let scrapePage = true;
+  while (scrapePage) {
+    for (const adElement of adElements) {
+      try {
+        const tableRows = await adElement.$$('.GO-Results-Data-Top tr');
+        const photoElement = await adElement.$('.GO-Results-Photo');
 
-    while (scrapePage) {
-      for (const adElement of adElements) {
-        try {
-          const tableRows = await adElement.$$('.GO-Results-Data-Top tr'); // This could be logged if necessary
-          const photoElement = await adElement.$('.GO-Results-Photo');
-
-          if (!photoElement) {
-            continue;
-          }
-          const [name, price, photoUrl, adUrl] = await Promise.all([
-            adElement.$eval('.GO-Results-Naziv', (el) => el.innerText.trim()),
-            adElement.$eval('.GO-Results-Price-Mid', (el) =>
-              el.innerText.trim(),
-            ),
-            photoElement.$eval('img', (el) => el.getAttribute('src')),
-            photoElement.$eval('a', (el) => el.getAttribute('href')),
-          ]);
-
-          const adId = adUrl.split('=')[1];
-          if (price === 'PRODANO') {
-            continue;
-          }
-
-          adData.push({
-            name,
-            price,
-            photoUrl,
-            adUrl,
-            adId,
-          });
-        } catch (error) {
+        if (!photoElement) {
           continue;
         }
-      }
-      const nextPageButton = await page.$('.GO-Rounded-R');
-      if (nextPageButton) {
-        const disabled = await page.evaluate(
-          (el) => el.classList.contains('disabled'),
-          nextPageButton,
-        );
-        if (disabled) {
-          scrapePage = false;
-          break;
-        }
-        const nextPageUrl = await page.evaluate(
-          (el) => el.querySelector('a').href,
-          nextPageButton,
-        );
+        const [name, price, photoUrl, adUrl] = await Promise.all([
+          adElement.$eval('.GO-Results-Naziv', (el) => el.innerText.trim()),
+          adElement.$eval('.GO-Results-Price-Mid', (el) => el.innerText.trim()),
+          photoElement.$eval('img', (el) => el.getAttribute('src')),
+          photoElement.$eval('a', (el) => el.getAttribute('href')),
+        ]);
 
-        await page.goto(nextPageUrl);
-        await page.waitForSelector('.GO-Results-Row');
-        adElements = await page.$$('.GO-Results-Row');
-      } else {
-        scrapePage = false;
+        const adId = adUrl.split('=')[1];
+        if (price === 'PRODANO') {
+          continue;
+        }
+
+        adData.push({
+          name,
+          price,
+          photoUrl,
+          adUrl,
+          adId,
+        });
+      } catch (error) {
+        continue;
       }
     }
+    const nextPageButton = await page.$('.GO-Rounded-R');
+    if (nextPageButton) {
+      const disabled = await page.evaluate(
+        (el) => el.classList.contains('disabled'),
+        nextPageButton,
+      );
+      if (disabled) {
+        scrapePage = false;
+        break;
+      }
+      const nextPageUrl = await page.evaluate(
+        (el) => el.querySelector('a').href,
+        nextPageButton,
+      );
 
-    return adData;
-  } catch (error) {
-    await saveList(error, 'error.json');
-
-    return [];
-  } finally {
-    await browser.close();
+      await page.goto(nextPageUrl);
+      await page.waitForSelector('.GO-Results-Row');
+      adElements = await page.$$('.GO-Results-Row');
+    } else {
+      scrapePage = false;
+    }
   }
+
+  await browser.close();
+  return adData;
 }
